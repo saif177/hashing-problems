64d2b29e0048b4f3ddea990f68b1b32f36aacccd
Saif Ali <saif.blt17@gmail.com>
2024-09-26 14:41:08 +0530

    use set() since array contains unique element

---
+++ is-subset-of-array.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def isSubSet(arr, subset):
+    set1 = set(arr)
+
+    for i in subset:
+        if i not in set1:
+            return False
+    return True
+
+
+arr = [1,2,3,4,5,6,7]
+subArr = [1,5,3]
+
+print(isSubSet(arr, subArr))
